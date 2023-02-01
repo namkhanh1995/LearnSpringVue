5b0be59b41573d7247f2f1c1adcd41146c935afa
khanh <namkhanh18051995@gmail.com>
2023-02-01 11:24:07 +0700

    add API getUserById

--- demo/src/main/kotlin/com/example/demo/controllers/Users.kt
+++ demo/src/main/kotlin/com/example/demo/controllers/Users.kt
@@ -2,28 +2,29 @@ package com.example.demo.controllers
 
 import com.example.demo.models.User
 import com.example.demo.services.UserService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api")
 class Users (
-        private val userService: UserService
+        private val userService: UserService? = null
 ){
     @GetMapping("/users")
     fun getAllUsers(): List<User> {
-       return userService.getAllUsers()
+       return userService!!.getAllUsers()
     }
     @PostMapping("/users")
     fun createUser(@RequestBody user:User): User? {
-        var id = userService.createUser(user)
-        return userService.getUser(user.id)
+        var id = userService!!.createUser(user)
+        return userService!!.getUser(user.id)
     }
 
-
+    @GetMapping("/users/{id}")
+    fun getUserById(@PathVariable id: Long?): ResponseEntity<User> {
+        val user = userService!!.getUser(id)
+        return ResponseEntity.ok(user)
+    }
 
 
 }
