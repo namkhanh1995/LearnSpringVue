ca487d7c4f258c3bcf99dfcf37740132ba921d70
khanh <namkhanh18051995@gmail.com>
2023-02-06 17:04:42 +0700

    edit timestamp

--- demo/src/main/kotlin/com/example/demo/services/ScheduleService.kt
+++ demo/src/main/kotlin/com/example/demo/services/ScheduleService.kt
@@ -5,9 +5,19 @@ import com.example.demo.models.Schedule
 import org.springframework.stereotype.Service
 
 @Service
-class ScheduleService (private val scheduleRepository: ScheduleRepository? = null){
+class ScheduleService (private val scheduleRepository: ScheduleRepository? = null,val offset: Int = 7){
     fun getAllSchedule(): List<Schedule> {
-        return scheduleRepository!!.findAll()
+        //MutableList(Schedule) lstSchedule = scheduleRepository!!.findAll();
+        //List<Schedule>() lstSchedule = scheduleRepository!!.findAll();
+       // schedule.firstTimestamp = schedule.firstTimestamp + (3600000*offset)
+        //return lstSchedule
+        var schedules = scheduleRepository!!.findAll()
+
+        for(schedule in schedules){
+            schedule.firstTimestamp = schedule.firstTimestamp + (3600000*offset)
+            schedule.secondTimestamp = schedule.secondTimestamp + (3600000*offset)
+        }
+        return schedules
     }
     fun createSchedule(schedule : Schedule) {
         return scheduleRepository!!.insert(schedule)
